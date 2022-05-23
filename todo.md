c2109e3e2f7088f3f423e417fb89696feaf134d9
morteza <mgahangir2006@gmail.com>
2022-05-23 16:15:15 +0430

    '13 - Our First Template'

---
+++ todo.md
@@ -0,0 +1,24 @@
+1. Tweets
+    -> Creating
+        -> Text
+        -> Image
+    -> Delete
+    -> Retweeting
+
+2. Users
+    -> Register
+    -> Login
+    -> Logout
+    -> Profile
+        -> Image?
+        -> Text?
+        -> Follow Button
+    -> Feed
+        -> Users feed only?
+        -> Users + who they follow?
+3. Following / Follwers
+
+Long term todos
+- Notifications
+- DM
+- Explore -> finding hastags
--- tweetme2/tweetme2/settings.py
+++ tweetme2/tweetme2/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tweetme2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'tweetme2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
