aa77276df04e9c6f9345612009b24eff11132fff
morteza <mgahangir2006@gmail.com>
2022-05-23 14:20:08 +0430

    '12 - Dynamic View into REST API Endpoint'

--- tweets/views.py
+++ tweets/views.py
@@ -1,16 +1,27 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from tweets.models import Tweet
 
 
-# Create your views here.
 def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """ 
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id = tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content}</h1>")
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status = status) #json.dums content_type='appliction/json'
+        
